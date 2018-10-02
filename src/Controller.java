d0247266af0f7e30c96f5c11d46f12ae2aafbb09
brokenClock <matti1902@gmail.com>
2018-10-02 15:18:20 +0200

    Translate from Morse Code to Letters works

--- src/Controller.java
+++ src/Controller.java
@@ -7,21 +7,33 @@ import sample.Translator.CodeTranslator;
 public class Controller {
     @FXML private TextArea textBoxRight;
     @FXML private TextArea textBoxLeft;
+    @FXML public Button toMorseCode, toLetters;
     private String textFromTextBoxLeft;
-    @FXML public Button translateButton;
-    private CodeTranslator codeTranslator;
+    public boolean toMorseButton;
 
     public Controller(){
 
     }
 
     public void initialize(){
-            translateButton.setOnAction(event -> {
-                textFromTextBoxLeft = textBoxLeft.getText();
-                setTextTextBoxLeft(textFromTextBoxLeft);
-                codeTranslator = new CodeTranslator(getTextFromTextBoxLeft());
-                textBoxRight.setText(codeTranslator.getYourText());
-            });
+        //edit TextArea(right) disabled
+        textBoxRight.setEditable(false);
+
+        toMorseCode.setOnAction(event -> {
+            toMorseButton = true;
+            buttonClickInstructions();
+        });
+        toLetters.setOnAction(event -> {
+            toMorseButton = false;
+            buttonClickInstructions();
+        });
+    }
+
+    private void buttonClickInstructions(){
+        textFromTextBoxLeft = textBoxLeft.getText();
+        setTextTextBoxLeft(textFromTextBoxLeft);
+        CodeTranslator codeTranslator = new CodeTranslator(getTextFromTextBoxLeft());
+        textBoxRight.setText(codeTranslator.getYourText());
     }
 
     private void setTextTextBoxLeft(String textFromTextBoxLeft) {
---
+++ src/Validation/InputValidation.java
@@ -0,0 +1,39 @@
+package sample.Validation;
+
+import javafx.scene.control.Alert;
+import sample.Base.SymbolsMorse;
+
+public class InputValidation {
+
+    private SymbolsMorse symbolsMorse = new SymbolsMorse();
+
+    public boolean validateTextToMorseCode(String yourText){
+        boolean[] characterIsOk = new boolean[yourText.length()];
+        boolean result = true;
+        for(int i = 0; i < yourText.length(); i++){
+            for (char characters : symbolsMorse.getSymbolsTableLetters()) {
+                if (String.valueOf(yourText.charAt(i)).equals(String.valueOf(characters)))
+                    characterIsOk[i] = true;
+            }
+        }
+
+        for(boolean characterIsInTheTable : characterIsOk){
+            System.out.println(characterIsInTheTable);
+            if(!characterIsInTheTable) {
+                result = false;
+                alertDialog();
+            }
+        }
+        return result;
+    }
+
+    public void alertDialog(){
+        Alert alertDialog = new Alert(Alert.AlertType.INFORMATION);
+        alertDialog.setTitle("Information");
+        alertDialog.setHeaderText(null);
+        alertDialog.setContentText("Your input contains illegal symbol, please check your sentence.");
+
+        alertDialog.showAndWait();
+    }
+
+}
